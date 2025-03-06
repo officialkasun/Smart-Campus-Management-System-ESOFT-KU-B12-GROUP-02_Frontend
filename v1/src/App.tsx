ac98ce7315d422efce8baddd5210076642189887
Kasun Madhusanka Jayasinghe <45454299+officialkasun@users.noreply.github.com>
2025-03-06 06:52:08 +0000

    updated and fixed the router path

--- v1/src/App.tsx
+++ v1/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import { Container } from '@mui/material';
 
 function App() {
-  
-
   return (
-    <>
+    
+    <div>
+          <Container>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -13,21 +17,10 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
-    </>
+    </Container>
+    </div>
+    
   )
 }
 
 export default App
-
-// Sample components for the routes
-function Home() {
-  return <h2>Home Page</h2>;
-}
-
-function About() {
-  return <h2>About Page</h2>;
-}
-
-function Contact() {
-  return <h2>Contact Page</h2>;
-}
