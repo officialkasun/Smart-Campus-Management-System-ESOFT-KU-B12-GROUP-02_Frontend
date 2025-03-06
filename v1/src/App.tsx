53b673944f48dfef1c666a2f97ec3db28b3d386e
Dilhan Chathuranga Thilakarathna <70667817+Dilhan-Infinity@users.noreply.github.com>
2025-03-06 08:01:02 +0000

    Create Navbar component and integrate it into App.tsx

--- v1/src/App.tsx
+++ v1/src/App.tsx
@@ -4,10 +4,14 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import { Container } from '@mui/material';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <Router>
+      {/* Add Navbar component here */}
+      <Navbar />
       <Container>
         <Routes>
           <Route path="/" element={<Home />} />
