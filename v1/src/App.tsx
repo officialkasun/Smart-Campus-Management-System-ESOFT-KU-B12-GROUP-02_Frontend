f509f46e0664c1eaf6f3fa149a8da70bbe2b8325
Kasun Madhusanka Jayasinghe <45454299+officialkasun@users.noreply.github.com>
2025-03-06 06:57:13 +0000

    bug fixed - Router Issue (react-router-dom) into (react-router)

--- v1/src/App.tsx
+++ v1/src/App.tsx
@@ -2,21 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+=======
+import { BrowserRouter as Router, Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import { Container } from '@mui/material';
+>>>>>>> 334eeaf (bug fixed - Router Issue (react-router-dom) into (react-router))
 
 function App() {
   
 
   return (
+<<<<<<< HEAD
     <>
       <Router>
+=======
+    <Router>
+      <Container>
+>>>>>>> 334eeaf (bug fixed - Router Issue (react-router-dom) into (react-router))
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+<<<<<<< HEAD
       </Router>
     </>
+=======
+      </Container>
+    </Router>
+>>>>>>> 334eeaf (bug fixed - Router Issue (react-router-dom) into (react-router))
   )
 }
 
