eb4bc12b5877e59bb0a429d9f4d12cd29dfc13f9
Kasun Madhusanka Jayasinghe <45454299+officialkasun@users.noreply.github.com>
2025-03-07 02:12:29 +0000

    Designed navbar and aligned to the right position with it's componenets

--- v1/src/App.tsx
+++ v1/src/App.tsx
@@ -1,18 +1,16 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Corrected import
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import { Container } from '@mui/material';
 import Navbar from './components/Navbar';
 
-
 function App() {
   return (
     <Router>
-      {/* Add Navbar component here */}
       <Navbar />
-      <Container>
+      <Container sx={{ marginTop: '64px' }}> {/* Adjust marginTop to match the height of the AppBar */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
