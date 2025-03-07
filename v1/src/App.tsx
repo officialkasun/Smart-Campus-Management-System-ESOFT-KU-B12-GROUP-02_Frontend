023da3e052f81d8323e6d698dc019f1a18911765
Kasun Madhusanka Jayasinghe <45454299+officialkasun@users.noreply.github.com>
2025-03-07 05:05:00 +0000

    Created Login form and authorization has been created for the mongodb. Also, installed some react libabries. Added browser coockie management functions. Relocated the pages and components. added authentication middleware.

--- v1/src/App.tsx
+++ v1/src/App.tsx
@@ -1,21 +1,26 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Error404 from './pages/Error404';
+import Home from './pages/Public/Home';
+import About from './pages/Public/About';
+import Contact from './pages/Public/Contact';
+import Error404 from './pages/Public/Error404';
 import { Container } from '@mui/material';
 import Navbar from './components/Navbar';
+import Login from './pages/Auth/Login';
+import Test from './pages/Test';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Container 
-      
+   
+      className='mt-[100px]'
       >
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Error404 />} />
