191fbe17bd906d6a32ba56289d234cc583b756d2
officialkasun <officialkasun@hotmail.com>
2025-03-12 12:34:57 +0530

    Added Password Change option to Account Manager and changed the theme to universal format of tailwindcss and create the authenticated route for Account Management

--- v1/src/components/ProtectedRoute.tsx
+++ v1/src/components/ProtectedRoute.tsx
@@ -10,11 +10,14 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element, allowedRole })
   // Read the user cookie
   const userCookie = Cookies.get('user');
   let userRole = '';
+  let userData = null;
   
   try {
-    if (userCookie) {
-      const userData = JSON.parse(userCookie);
+    if (userCookie) {//Check if user cookie exists
+       userData = JSON.parse(userCookie);
       userRole = userData.role;
+    }else{
+      return <Navigate to="/login" replace />;
     }
   } catch (error) {
     console.error('Error parsing user cookie:', error);
@@ -22,6 +25,8 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element, allowedRole })
 
   // If user has the correct role, render the requested page, otherwise redirect to login
   if (userRole === allowedRole) {
+    return element;
+  }else if(allowedRole === "" && userData ){ // If no role is specified, render the requested page
     return element;
   } else {
     return <Navigate to="/login" replace />;
