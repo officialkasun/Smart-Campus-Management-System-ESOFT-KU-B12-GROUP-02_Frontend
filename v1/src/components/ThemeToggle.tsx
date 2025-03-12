ca099f2f0cda7835698470ae4f1faec81a95896c
Kasun Madhusanka Jayasinghe <officialkasun@officialkasun.local>
2025-03-12 12:34:57 +0530

    Added Password Change option to Account Manager and changed the theme to universal format of tailwindcss and create the authenticated route for Account Management

--- v1/src/components/ThemeToggle.tsx
+++ v1/src/components/ThemeToggle.tsx
@@ -1,19 +1,22 @@
 import { IconButton, Tooltip } from '@mui/material';
-import { useTheme } from '../context/ThemeContext';
 import Brightness4Icon from '@mui/icons-material/Brightness4'; // Dark mode icon
 import Brightness7Icon from '@mui/icons-material/Brightness7'; // Light mode icon
 
-const ThemeToggle = () => {
-  const { theme, toggleTheme } = useTheme();
-  
+// Define the props interface
+export interface ThemeToggleProps {
+  currentTheme: string;
+  toggleTheme: () => void;
+}
+
+const ThemeToggle: React.FC<ThemeToggleProps> = ({ currentTheme, toggleTheme }) => {
   return (
-    <Tooltip title={theme === 'light' ? 'Switch to dark mode' : 'Switch to light mode'}>
+    <Tooltip title={currentTheme === 'light' ? 'Switch to dark mode' : 'Switch to light mode'}>
       <IconButton 
         onClick={toggleTheme} 
         color="inherit"
         className="theme-toggle-button text-primary-content"
       >
-        {theme === 'light' ? 
+        {currentTheme === 'light' ? 
           <Brightness4Icon className="text-primary-content" /> : 
           <Brightness7Icon className="text-yellow-300" />
         }
