f0041e5d71fb8ad42e0917467f8043414fb9f4f8
Dilhan C Thilakarathna <dctspeeder@gmail.com>
2025-03-19 16:55:15 +0530

    create Courses.tsx component with initial structure Relates to #11

---
+++ v1/src/pages/Features/Lecturer/Courses.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Courses: React.FC = () => {
+  const [courses, setCourses] = useState([]);
+
+  return (
+    <div>
+      <h1>Available Courses</h1>
+    </div>
+  );
+};
+
+export default Courses;
