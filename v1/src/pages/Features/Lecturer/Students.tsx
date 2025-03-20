cbc9f9a24cdeca9b842a971e15ac8e41464465ba
Dilhan C Thilakarathna <dctspeeder@gmail.com>
2025-03-20 10:56:22 +0530

    add necessory imports for Students component Relates to #18

---
+++ v1/src/pages/Features/Lecturer/Students.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import config from '../../../config';
+import { motion } from 'framer-motion';
+import { 
+  Typography, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
+  TablePagination, Chip, IconButton, Box, Modal, Card, CardContent, TextField, Divider,
+  Avatar, CircularProgress, Alert, InputAdornment, Tooltip, TableSortLabel
+} from '@mui/material';
+
+import { 
+  Visibility as VisibilityIcon, Person as PersonIcon, Email as EmailIcon, Badge as BadgeIcon,
+  School as SchoolIcon, AccessTime as AccessTimeIcon, Search as SearchIcon, Clear as ClearIcon,
+  Refresh as RefreshIcon
+} from '@mui/icons-material';
